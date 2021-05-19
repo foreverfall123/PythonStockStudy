0e97624f38ae969ba72b9ed5a635f8a5def8233a
foreverfall123 <foreverfall@naver.com>
2021-05-19 21:59:13 +0900

    change_word

--- getLastPageNum.py
+++ getLastPageNum.py
@@ -4,4 +4,6 @@ import requests
 url = 'https://finance.naver.com/item/sise_day.nhn?code=068270&page=1'
 html = BeautifulSoup(requests.get(url, headers={'User-agent': 'Mozilla/5.0'}).text, 'lxml')
 pgrr = html.find('td', class_='pgRR')
-print(pgrr.a['href'])
+s = str(pgrr.a['href']).split('=')
+last_page = s[-1]
+print(last_page)
