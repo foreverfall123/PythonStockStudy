13e0640284861a604660f130097c373167e30689
foreverfall123 <foreverfall@naver.com>
2021-05-24 22:37:16 +0900

    Get Code Logic

--- myPackage/DBUpdater.py
+++ myPackage/DBUpdater.py
@@ -43,6 +43,13 @@ class DBUpdater:
     def read_krx_code(self):
         """KRX로부터 상장법인목록 파일을 읽어와서 데이터 프레임으로 변환"""
 
+        url = 'http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13'
+        krx = pd.read_html(url, header=0)[0]
+        krx = krx[['종목코드', '회사명']]
+        krx = krx.rename(columns={'종목코드':'code', '회사명':'company'})
+        krx.code = krx.code.map('{:06d}'.format)
+        return krx
+
     def update_comp_info(self):
         """종목 코드를 company_info 테이블에 업데이트 한 후 딕셔너리에 저장"""
 
